02c3e69b28680ee1c26234255f5773b30f8b1b97
Peilin Yang <yang.peilinc@gmail.com>
2020-06-22 21:12:30 +0800

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,7 +2,7 @@ CREATE DATABASE test1
 	ON
 	(
 	NAME="test1",
-	FILENAME="C:\Shengcai\test2.mdf",
+	FILENAME="C:~test2.mdf",
 	SIZE=2MB,
 	MAXSIZE=50MB,
 	FILEGROWTH=10%
@@ -10,7 +10,7 @@ CREATE DATABASE test1
 	LOG ON
 	(
 		NAME="test_log",
-		FILENAME="C:\Shengcai\test2.ldf",
+		FILENAME="C:~\test2.ldf",
 		SIZE=2MB,
 		MAXSIZE=5MB,
 		FILEGROWTH=1MB
